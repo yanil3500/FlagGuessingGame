c14732a9c3538896e783d6035d2825d666583488
yanil3500 <yanil3500@gmail.com>
2020-02-05 14:51:09 -0500

    Initial Commit

---
+++ FlagGuessingGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlagGuessingGame
+//
+//  Created by Elyanil Liranzo Castro on 2/5/20.
+//  Copyright © 2020 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
